f7157a17374d4ac82b6300e34a54432f988ef755
Celso Lopes <celso.lopes.jr@gmail.com>
2022-11-04 21:34:13 -0300

    Add/read list. UseState and use UseEffect. API

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import Home from './pages/home'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './styles/global.css';
+import {Home} from './pages/home';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
